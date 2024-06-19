98951743b23718d2415778b706f16399cc45d4db
Emmanuel Diomande <emmanuel.diomande@outlook.com>
2024-06-19 20:05:35 +0000

    Version App 0.3.1 JCI - Séguéla

--- .output/public/_nuxt/CecShe0u.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,a as n}from"./DlhhaDuR.js";import{_ as s}from"./DlAUqK2U.js";import{c as r,b as e,F as c,a as _,o as m}from"./Fi5lLTPs.js";import"./D8N9wp3U.js";const d={},f=_("div",{class:"mt-48 w-11/12 text-4xl font-exo font-bold m-auto"},"Partenaires Page",-1);function i(l,p){const o=a,t=n;return m(),r(c,null,[e(o),f,e(t)],64)}const b=s(d,[["render",i]]);export{b as default};
